eb2b42d6c7ace7d0ff74d9e0e8484b4648b6568b
sanjidur <sanjidur@gmail.com>
2024-02-23 20:50:22 +0600

    Changed pages titles, fixed footer image, added new page

--- static/homepage.js
+++ static/homepage.js
@@ -55,3 +55,31 @@ addEventListener("load",()=>{
     intro.classList.add("loaded");
     logo.classList.add("loaded");
 })
+
+function crossScroll() {
+        var sections = document.querySelectorAll('.section');
+
+        sections.forEach(function(section) {
+            section.addEventListener('wheel', function (event) {
+                let getstyle = window.getComputedStyle(section);
+                const flexDirection = getstyle.getPropertyValue('flex-direction');
+                
+                if(flexDirection !== "row") return;
+                else {
+                    event.preventDefault();
+                    var scrollAmount = event.deltaY*0.5;
+                    section.scrollTop += scrollAmount;
+
+                    if (scrollAmount < 0) {
+                        // Scroll Up
+                        section.scrollLeft -= 15;
+                    } else {
+                        section.scrollLeft += 15;
+                    }}
+            })
+        })
+}
+
+if (innerWidth < 1081){
+    crossScroll();
+}
